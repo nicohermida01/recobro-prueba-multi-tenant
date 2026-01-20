5699cdc2ec74ead7c1f4c8f9c3eb2e896419dd16
Nicolas Hermida <hermida.nicolas101@gmail.com>
2026-01-20 12:37:50 -0300

    Agrego funcion para obtener cantidad de proyectos de un tenant en project service

--- services/projects.service.ts
+++ services/projects.service.ts
@@ -11,3 +11,9 @@ export async function getOneProjectById(projectId: string) {
 		PROJECTS_TABLE.find(project => project.id === projectId) || null,
 	)
 }
+
+export async function getCountProjectsByTenant(tenantId: string) {
+	return await Promise.resolve(
+		PROJECTS_TABLE.filter(project => project.tenantId === tenantId).length,
+	)
+}
