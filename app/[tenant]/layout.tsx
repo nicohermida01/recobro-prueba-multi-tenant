556ced8b3a345da6fb9867873d6e985636541bbf
Nicolas Hermida <hermida.nicolas101@gmail.com>
2026-01-20 12:34:50 -0300

    actualizo el layout del tenant para incluir el nombre del tenant en el header

--- app/[tenant]/layout.tsx
+++ app/[tenant]/layout.tsx
@@ -1,14 +1,19 @@
 import { TenantHeader } from '@/components/TenantHeader'
+import { getOneTenantBySlug } from '@/services/tenants.service'
 
-export default function TenantLayout({
+export default async function TenantLayout({
 	children,
+	params,
 }: {
 	children: React.ReactNode
+	params: Promise<{ tenant: string }>
 }) {
+	const { tenant } = await params
+	const tenantData = await getOneTenantBySlug(tenant)
+
 	return (
 		<div>
-			<TenantHeader />
-
+			<TenantHeader tenant={tenantData?.name || ''} />
 			<section>{children}</section>
 		</div>
 	)
--- components/TenantHeader.tsx
+++ components/TenantHeader.tsx
@@ -1,9 +1,15 @@
 import { BackButton } from './BackButton'
 
-export function TenantHeader() {
+interface Props {
+	tenant: string
+}
+
+export function TenantHeader({ tenant }: Props) {
 	return (
-		<header className='border-b-2 border-slate-500 py-4 px-10'>
+		<header className='border-b-2 border-slate-500 py-4 px-10 flex justify-between items-center'>
 			<BackButton />
+
+			<h2 className='font-bold text-lg'>{tenant}</h2>
 		</header>
 	)
 }
