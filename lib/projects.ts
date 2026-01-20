e6df8fe79a14de8dc7f2585710b916731eb505f2
Nicolas Hermida <hermida.nicolas101@gmail.com>
2026-01-20 14:27:45 -0300

    Arreglo import

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,10 +1,10 @@
-import { Project, ProjectStatus } from '@/types/project.type'
+import { Project, ProjectStatusEnum } from '@/types/project.type'
 
 export const PROJECTS_TABLE: Project[] = [
 	{
 		id: '1',
 		name: 'Sitio Web Corporativo',
-		status: ProjectStatus.ACTIVE,
+		status: ProjectStatusEnum.ACTIVE,
 		tenantId: '1',
 		content:
 			'Irure amet eu dolore magna adipisicing consectetur id dolore exercitation velit esse dolore elit Lorem. Ullamco ipsum dolor sint culpa. Cupidatat sint occaecat velit adipisicing elit elit labore consequat aliqua cillum amet.',
@@ -12,7 +12,7 @@ export const PROJECTS_TABLE: Project[] = [
 	{
 		id: '2',
 		name: 'Aplicación Móvil',
-		status: ProjectStatus.ARCHIVED,
+		status: ProjectStatusEnum.ARCHIVED,
 		tenantId: '1',
 		content:
 			'Irure amet eu dolore magna adipisicing consectetur id dolore exercitation velit esse dolore elit Lorem. Ullamco ipsum dolor sint culpa. Cupidatat sint occaecat velit adipisicing elit elit labore consequat aliqua cillum amet.',
@@ -20,7 +20,7 @@ export const PROJECTS_TABLE: Project[] = [
 	{
 		id: '3',
 		name: 'Plataforma de E-commerce',
-		status: ProjectStatus.ACTIVE,
+		status: ProjectStatusEnum.ACTIVE,
 		tenantId: '2',
 		content:
 			'Irure amet eu dolore magna adipisicing consectetur id dolore exercitation velit esse dolore elit Lorem. Ullamco ipsum dolor sint culpa. Cupidatat sint occaecat velit adipisicing elit elit labore consequat aliqua cillum amet.',
